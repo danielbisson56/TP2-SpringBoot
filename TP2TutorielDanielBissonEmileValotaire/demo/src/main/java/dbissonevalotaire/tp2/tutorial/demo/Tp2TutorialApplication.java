48bb959fa6e1e411452588c5ecee739c150acde7
danielbisson56 <43545496+danielbisson56@users.noreply.github.com>
2019-11-09 21:35:27 -0500

    Add files via upload
    Tout les fichiers du projet en model MVC, BD REST avec serveur apache tomcat.

---
+++ TP2TutorielDanielBissonEmileValotaire/demo/src/main/java/dbissonevalotaire/tp2/tutorial/demo/Tp2TutorialApplication.java
@@ -0,0 +1,13 @@
+package dbissonevalotaire.tp2.tutorial.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2TutorialApplication.class, args);
+	}
+
+}
---
+++ TP2TutorielDanielBissonEmileValotaire/demo/src/main/java/dbissonevalotaire/tp2/tutorial/demo/model/Person.java
@@ -0,0 +1,25 @@
+package dbissonevalotaire.tp2.tutorial.demo.model;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Person {
+
+	private final UUID id;
+	private final String name;
+	
+	public Person(@JsonProperty("id") UUID id,
+				 	@JsonProperty("name") String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}	
+}
