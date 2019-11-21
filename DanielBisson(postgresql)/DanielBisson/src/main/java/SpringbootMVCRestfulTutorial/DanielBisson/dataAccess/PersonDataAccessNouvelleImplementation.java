fa50ded77289e15359ee6e83aa58f588fa74c372
Daniel Bisson <danielbisson56@gmail.com>
2019-11-20 19:47:39 -0500

    PostgreSQL finalisé avec Readme

--- DanielBisson(postgresql)/DanielBisson/src/main/java/SpringbootMVCRestfulTutorial/DanielBisson/dataAccess/PersonDataAccessNouvelleImplementation.java
+++ DanielBisson(postgresql)/DanielBisson/src/main/java/SpringbootMVCRestfulTutorial/DanielBisson/dataAccess/PersonDataAccessNouvelleImplementation.java
@@ -1,10 +1,11 @@
 package SpringbootMVCRestfulTutorial.DanielBisson.dataAccess;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import SpringbootMVCRestfulTutorial.DanielBisson.model.Person;
@@ -12,6 +13,13 @@ import SpringbootMVCRestfulTutorial.DanielBisson.model.Person;
 @Repository("nouvelleImplementation")
 public class PersonDataAccessNouvelleImplementation implements PersonDataAccessInterface {
 
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public PersonDataAccessNouvelleImplementation(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
 	@Override
 	public int insertPerson(UUID id, Person person) {
 		// TODO Auto-generated method stub
@@ -25,10 +33,15 @@ public class PersonDataAccessNouvelleImplementation implements PersonDataAccessI
 	}
 
 	@Override
-	public List<Person> selectAllPeople() {
-		List<Person> people = new ArrayList<Person>();
+	public List<Person> selectAllPeople(){
+		final String sql = "SELECT id, name FROM person;";
 		
-		people.add(new Person(UUID.randomUUID(),"Ceci est ma nouvelle implémentation et ça fonctionne!"));
+		List<Person> people = jdbcTemplate.query(					
+				sql, (resultSet, i) -> {
+				UUID id = UUID.fromString(resultSet.getString("id"));
+				String name = resultSet.getString("name");
+				return new Person(id, name);						
+				});
 		return people;
 	}
 
@@ -46,8 +59,17 @@ public class PersonDataAccessNouvelleImplementation implements PersonDataAccessI
 
 	@Override
 	public Optional<Person> selectPersonById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		final String sql = "SELECT id, name FROM person WHERE id = ?";
+		Person person = 
+				jdbcTemplate.queryForObject(					
+				sql,
+				new Object[] {id},
+				(resultSet, i) -> {
+				UUID personId = UUID.fromString(resultSet.getString("id"));
+				String name = resultSet.getString("name");
+				return new Person(personId, name);			
+				});
+		return Optional.ofNullable(person);
 	}
 
 }
